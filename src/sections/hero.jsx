80e27ff4434b74cbf7b1a75163dbcdce74e16974
einnaa <einnajoyc003@gmail.com>
2026-02-21 17:53:51 +0800

    bg color

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -4,8 +4,7 @@ import downArrow from "../assets/down-arrow.png";
 
 export default function Hero() {
   return (
-    <section className="min-h-screen flex flex-col items-center justify-center relative overflow-hidden px-6 py-24 md:py-32">
-      <div className="absolute inset-0 bg-linear-to-br from-vintage-orange/15 to-vintage-bg z-0" />
+    <section className="min-h-screen flex flex-col items-center justify-center relative overflow-hidden px-6 py-24 md:py-32 bg-linear-to-b from-vintage-orange/6 to-vintage-bg">
       <div className="z-10 w-full max-w-xs md:max-w-lg md:top-10 mx-auto animate-fade-in">
         <div className="w-full overflow-hidden">
           <img 
