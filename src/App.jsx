8f655187e6702097f87a22caeb7cf471c636bd68
einnaa <einnajoyc003@gmail.com>
2026-03-03 17:31:39 +0800

    chore: config vercel

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ProjectDetails from "./pages/ProjectDetails";
 import AllProjects from "./pages/AllProjects";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/all-projects" element={<AllProjects />} />
         <Route path="/projects/:id" element={<ProjectDetails />} />
       </Routes>
+      <Analytics />
     </Router>
   );
 }
